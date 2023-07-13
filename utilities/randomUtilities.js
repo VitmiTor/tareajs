aefc303fccc2a967965affb6f306cee5f5455df7
VitmiTor <torremvictor@gmail.com>
2023-07-12 23:00:06 -0500

    ultimatoum

--- utilities/randomUtilities.js
+++ utilities/randomUtilities.js
@@ -1,23 +1,23 @@
 "use strict";
 
 /**
- * 
- * @param {Number} min 
- * @param {Number} max 
- * @returns 
+ *
+ * @param {Number} min
+ * @param {Number} max
+ * @returns
  */
-const generarEnteroAleatorio=(min,max)=>{
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const generarEnteroAleatorio = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 /**
- * 
- * @param {Number} min 
- * @param {Number} max 
- * @returns 
+ *ß
+ * @param {Number} min
+ * @param {Number} max
+ * @returns
  */
-const generarDecimalAleatorio=(min,max)=>{
-    return Math.random() * (max-min+1)+min;
-}
+const generarDecimalAleatorio = (min, max) => {
+  return Math.random() * (max - min + 1) + min;
+};
 
-
-module.exports={generarDecimalAleatorio,generarEnteroAleatorio};
+module.exports = { generarDecimalAleatorio, generarEnteroAleatorio };
